28007acb85a36c186a248b4e85c4b30abd401ea7
aurelsensei <aurelien.ursulet@gmail.com>
2021-04-13 11:02:12 +0200

    suite test

---
+++ src/CustomEntity.php
@@ -0,0 +1,11 @@
+<?php
+namespace EnergySolution\ProjectEntity;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CustomEntity extends Bundle
+{
+//    public function greet($greet = "Hello World")
+//    {
+//        return $greet;
+//    }
+}
